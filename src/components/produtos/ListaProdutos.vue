e9dfa94c0a1e262403559edff58ac817a2e0ed92
eric_motta <eric@greenn.com.br>
2023-09-06 19:55:43 -0300

    ok

---
+++ src/components/produtos/ListaProdutos.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <h1>Lista de produtos</h1>
+    <b-row class="table-header bg-dark text-white">
+      <b-col>#</b-col>
+      <b-col>Nome</b-col>
+      <b-col>Valor</b-col>
+      <b-col>Descrição</b-col>
+      <b-col>Ações</b-col>
+    </b-row>
+    {{ produtos }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      teste: "ok",
+      produtos: [],
+    };
+  },
+  methods: {
+    buscaProdutos() {
+      fetch("http://192.168.0.120:8001/api/product")
+        .then((data) => {
+          return data.json();
+        })
+        .then((produtos) => {
+          this.produtos = produtos.data;
+        });
+    },
+  },
+  created() {
+    // executa coias antes do html ser rendizerado
+    this.buscaProdutos();
+  },
+};
+</script>
+
+<style>
+</style>
