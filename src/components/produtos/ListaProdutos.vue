842b04ac7e169ca3f015686bc97749745a5c807f
eric_motta <eric@greenn.com.br>
2023-09-08 21:22:38 -0300

    ok

---
+++ src/components/produtos/ListaProdutos.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <h1 class="mb-4">Listagem de produtos</h1>
+    <b-row class="p-2 bg-dark text-light">
+      <b-col>#</b-col>
+      <b-col>Nome</b-col>
+      <b-col>Valor</b-col>
+      <b-col>Decrição</b-col>
+      <b-col>Ações</b-col>
+    </b-row>
+
+    <b-row v-for="(produto, index) in produtos" :key="index" class="p-2">
+      <b-col>{{ produto.id }}</b-col>
+      <b-col>{{ produto.name }}</b-col>
+      <b-col>{{ produto.value }}</b-col>
+      <b-col>{{ produto.description }}</b-col>
+      <b-col>
+        
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      exemplo: "exemplo de variavel vue",
+      produtos: [],
+    };
+  },
+  methods: {
+    buscaProdutos() {
+      fetch("http://192.168.0.120:8001/api/product")
+        .then((data) => {
+          return data.json();
+        })
+        .then((data) => {
+          this.produtos = data.data;
+        });
+    },
+  },
+  created() {
+    this.buscaProdutos();
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ListaProdutos/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import ListaProdutos from '@/components/produtos/ListaProdutos.vue';
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  components:{
+    ListaProdutos
   }
 }
 </script>
